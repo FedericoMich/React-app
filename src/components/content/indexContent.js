ea9e2c1e161eed31dfaba1fea5eded3b1b09b220
federico <federico.mich97@gmail.com>
2021-10-29 10:45:06 +0200

    step 3

--- src/components/content/indexContent.js
+++ src/components/content/indexContent.js
@@ -4,9 +4,18 @@ import "./indexContent.css"
 export const IndexContent = () => {
     return (
         <div className="mainContainer">
-            
-
-            
+            <div className="totalContainer">
+                <h4 className="total">Totale repos: 10</h4>
+            </div>
+            <div className="content">
+            </div>
+            <div className="pageCount">
+                <div className="blueButtom">
+                    <h4 className="buttom">precendente</h4>
+                    <h4 className="buttom">2 di 4</h4>
+                    <h4 className="buttom">Successivo</h4>
+                </div>
+            </div>
         </div>
     )
 }
