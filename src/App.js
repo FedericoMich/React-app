883cac2adaefb4e01dff850268e0ad5b9e9a5589
federico <federico.mich97@gmail.com>
2021-11-08 07:52:06 +0100

    V 0.0.3 add console log apiMerge

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Head } from './components/head/Head';
 import { IndexContent } from './components/content/indexContent';
+import { ListRepos } from './api/repos';
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Head/>
       <IndexContent/>
+      <ListRepos/>
     </div>
   );
 }
---
+++ src/api/repos.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import students from './mock/apiMerge.json'
+
+
+export const ListRepos = () => {
+    const repos = students
+    console.log(repos)
+    return (
+        <div>
+        </div>
+    )
+}
