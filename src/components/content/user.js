7f66013e6fc0071e6dfb2a70f994fb370ee5caa2
federico <federico.mich97@gmail.com>
2021-12-04 19:19:05 +0100

    V 0.2.6

--- src/components/content/user.js
+++ src/components/content/user.js
@@ -5,7 +5,6 @@ import Collapse from '@mui/material/Collapse';
 import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
